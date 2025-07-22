9b48353a08fcc5ee34eac326e5bac62b191b0d99
Pandi <pandirotama@gmail.com>
2025-07-22 17:11:46 +0700

    add allure code for the report

--- tests/test_company_registration.py
+++ tests/test_company_registration.py
@@ -1,5 +1,6 @@
 import pytest
 import time
+import allure
 
 from utils.driver_setup import init_driver
 from page.login import LoginPage
@@ -12,22 +13,31 @@ def driver():
     yield driver
     driver.quit()
 
-
+@allure.feature("Login")
+@allure.story("User can login successfully")
 def test_login(driver):
     login_page = LoginPage(driver)
 
-    login_page.enter_company_id()
-    login_page.enter_username()
-    login_page.enter_password()
-    login_page.tap_sign_in()
+    with allure.step("Fill login form"):
+        login_page.enter_company_id()
+        login_page.enter_username()
+        login_page.enter_password()
+
+    with allure.step("Tap Sign In"):
+        login_page.tap_sign_in()
 
     time.sleep(3)
 
+@allure.feature("Registration")
+@allure.story("User can register new customer")
 def test_company_registration(driver):
     reg = RegistrationPage(driver)
 
-    reg.new_costumer_menu()
-    reg.new_costumer_registration()
-    reg.fill_form()
+    with allure.step("Open new customer menu"):
+        reg.new_costumer_menu()
+    with allure.step("Open registration form"):
+        reg.new_costumer_registration()
+    with allure.step("Fill registration form"):
+        reg.fill_form()
 
     time.sleep(5)
